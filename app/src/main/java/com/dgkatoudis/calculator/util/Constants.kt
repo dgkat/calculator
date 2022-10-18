d3541f4ef0cf630d68184298b5e79319802fa495
dgkat <dgkatoudis@gmail.com>
2022-10-18 21:44:18 +0300

    Final commit

--- app/src/main/java/com/dgkatoudis/calculator/util/Constants.kt
+++ app/src/main/java/com/dgkatoudis/calculator/util/Constants.kt
@@ -2,5 +2,5 @@ package com.dgkatoudis.calculator.util
 
 object Constants {
     const val BASE_URL = "https://api.apilayer.com/fixer/"
-    const val API_KEY = "YOUR_API_KEY"
+    const val API_KEY = "E3S82AUhAEkir8pcaFztFOB2jjZfooUP"
 }
